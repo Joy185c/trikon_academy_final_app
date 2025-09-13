36fdd61c310825ab50c225b821b13d13bcd3b219
Joy sarkar <joy185c@gmail.com>
2025-09-14 05:03:26 +0600

    fix: clean install and build

--- src/lib/supabaseclient.js
+++ src/lib/supabaseclient.js
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://edmbobiqqgdzakbqxmnc.supabase.co"; 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbWJvYmlxcWdkemFrYnF4bW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxMzcyNTQsImV4cCI6MjA3MjcxMzI1NH0.edmkXa309gM1g0j8fb3obucn9P0KAxXkTK6FrbiosbE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
