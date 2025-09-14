9f68118aa992315b01ad60c8c5f339cf60d822df
Joy sarkar <joy185c@gmail.com>
2025-09-14 13:45:08 +0600

    Fix: added .jsx extension for AuthContext import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; // ✔️ ঠিক আছে
+import { AuthProvider } from "./context/AuthContext.jsx"; // ✅ extension যোগ করা হলো
 import "./index.css";
 
 // KaTeX CSS for math formulas
